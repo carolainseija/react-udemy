e8c74aa85ff6e8e5ce70ec2a526051128952dde0
carolainseija <carolainsilva1@gmail.com>
2022-09-21 10:02:27 -0300

    archivos iniciales

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import './index.css';
+
+
+const App = () => {
+
+
+  return (
+    <>
+      <h1>react</h1>
+    </>
+  );
+};
+
+export default App;
